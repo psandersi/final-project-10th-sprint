13fd5ab4bc7fe7d36ba5b5bb2121dbdb8ded6af9
Daria Galushko <dashuta.galushko@mail.ru>
2024-05-25 16:54:48 +0500

    fourth

--- precode.sh
+++ precode.sh
@@ -19,16 +19,14 @@ echo "$1, привет!"' > dir2/hello.sh
 # устанавливаем для task/dir2/hello.sh права rwxrw-r--
 chmod 764 dir2/hello.sh
 # сохраняем список файлов task/dir2 в task/dir2/list.txt
-touch dir2/list.txt
-ls -r > list.txt
-cd ..
+ls dir2 > dir2/list.txt
 # копируем содержимое каталога task/dir2 в каталог task/dir3/dir4
 cp -r dir2 dir3/dir4
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
 find -iname '*.txt' >  dir1/summary.txt
 # дописываем в task/dir1/summary.txt содержимое task/dir2/list.txt
-cat dir1/summary.txt >> dir2/list.txt
+cat dir2/list.txt >> dir1/summary.txt
 # определяем переменную окружения NAME со значением "Всем студентам"
 NAME="Всем студентам"
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
